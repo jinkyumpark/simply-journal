00785f5b5f23aff32aa47ae1a6ca2cb3ab86dd4c
jinkyumpark <jinpark1025@gmail.com>
2022-09-06 05:54:52 +0900

    add project using spring intilizr

---
+++ OccurenceJournal/src/main/java/com/jinkyumpark/occurencejournal/OccurenceJournalApplication.java
@@ -0,0 +1,13 @@
+package com.jinkyumpark.occurencejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccurenceJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccurenceJournalApplication.class, args);
+	}
+
+}
